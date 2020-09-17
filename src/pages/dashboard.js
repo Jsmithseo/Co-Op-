74dc2b6c56d9ae56f378ccf9d08ffd7090f8f822
Jeremy Smith <jeremy.smith@pantheon.io>
2020-09-17 15:59:49 -0700

    WIP

---
+++ src/pages/dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import Layout from '../components/layout'
+
+
+
+const Dashboard = () => {
+    return (
+        <Layout>
+            <p>TODO: Create Dashboard</p>
+        </Layout>
+    )
+}
+
+
+
+export default Dashboard
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
 
 
 const IndexPage = () => (
-<>
 <Layout>  
 <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-    <Link to="/dashboard/">Dashboard</Link> <br />
+    <Link to="/dashboard">Dashboard</Link> <br />
 </Layout>
 
-    </>
 )
 
 export default IndexPage
