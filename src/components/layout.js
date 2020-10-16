cb9e59340f3709edd7c932a252df2a5fe273ff6f
Jeremy Smith <jeremy.smith@pantheon.io>
2020-10-16 15:29:55 -0700

    WIP

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,57 @@ import React from "react"
 import { Link } from "gatsby"
 import { IdentityContextProvider } from "react-netlify-identity-widget";
 import "./layout.css"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Button from "react-bootstrap/Button"
 
 const Layout = ({ children }) => (
+  <>
   <IdentityContextProvider url="https://marin-city-co-op.netlify.app">
     <header>
-      <Link to="/">Co Op App</Link>
+      <Link to="/"></Link>
+          <Link className="link" to="/support">
+            <Button variant="outline-success">Donate</Button>
+          </Link>
+          <Link className="link" to="/dashboard"><Button>Login</Button></Link>
     </header>
+    <Navbar
+        collapseOnSelect
+        expand="md"
+        fixed="top"
+        style={{
+          padding: "0 0!important",
+        }}
+      >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          style={{
+            backgroundColor: "white",
+          }}
+        >
+          <Nav className="mr-auto">
+            <Link className="link" to="/">
+              Home
+            </Link>
+            <Link className="link" to="/about">
+              Teams
+            </Link>
+            <Link className="link" to="/programs">
+              Our Vision
+            </Link>
+            <Link className="link" to="/support">
+              Our Leaders
+            </Link>
+            <Link className="link" to="/contact">
+              Contact Us
+            </Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Navbar>
     <main>{children}</main>
   </IdentityContextProvider>
+  </>
 )
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-
+import Hero from "../components/hero"
 const IndexPage = () => (
+  <>
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
-    <Link to="/dashboard">Dashboard</Link> <br />
   </Layout>
+  <Hero/>
+  </>
 )
 
 export default IndexPage
